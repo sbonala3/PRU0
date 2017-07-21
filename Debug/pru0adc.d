da6daabde8c66977e369d34bc8f23b14f422235d
sbonala3 <sbonala3@gatech.edu>
2017-07-20 20:58:30 -0500

    First commit

---
+++ Debug/pru0adc.d
@@ -0,0 +1,33 @@
+# FIXED
+
+pru0adc.obj: ../pru0adc.c
+pru0adc.obj: ../resource_table_0.h
+pru0adc.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-pru_2.1.5/include/stddef.h
+pru0adc.obj: C:/Users/a0232053/AppData/Local/Microsoft/Windows/INetCache/IE/1VP0HB23/pru-software-support-package-pru-software-support-package-master/pru-software-support-package-pru-software-support-package/include/rsc_types.h
+pru0adc.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-pru_2.1.5/include/stdint.h
+pru0adc.obj: C:/Users/a0232053/AppData/Local/Microsoft/Windows/INetCache/IE/1VP0HB23/pru-software-support-package-pru-software-support-package-master/pru-software-support-package-pru-software-support-package/include/pru_types.h
+pru0adc.obj: C:/Users/a0232053/AppData/Local/Microsoft/Windows/INetCache/IE/1VP0HB23/pru-software-support-package-pru-software-support-package-master/pru-software-support-package-pru-software-support-package/include/pru_virtio_ids.h
+pru0adc.obj: C:/Users/a0232053/AppData/Local/Microsoft/Windows/INetCache/IE/1VP0HB23/pru-software-support-package-pru-software-support-package-master/pru-software-support-package-pru-software-support-package/include/am335x/pru_cfg.h
+pru0adc.obj: C:/Users/a0232053/AppData/Local/Microsoft/Windows/INetCache/IE/1VP0HB23/pru-software-support-package-pru-software-support-package-master/pru-software-support-package-pru-software-support-package/include/am335x/pru_intc.h
+pru0adc.obj: C:/Users/a0232053/AppData/Local/Microsoft/Windows/INetCache/IE/1VP0HB23/pru-software-support-package-pru-software-support-package-master/pru-software-support-package-pru-software-support-package/include/pru_rpmsg.h
+pru0adc.obj: C:/Users/a0232053/AppData/Local/Microsoft/Windows/INetCache/IE/1VP0HB23/pru-software-support-package-pru-software-support-package-master/pru-software-support-package-pru-software-support-package/include/am335x/../pru_virtqueue.h
+pru0adc.obj: C:/Users/a0232053/AppData/Local/Microsoft/Windows/INetCache/IE/1VP0HB23/pru-software-support-package-pru-software-support-package-master/pru-software-support-package-pru-software-support-package/include/pru_virtio_ring.h
+pru0adc.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-pru_2.1.5/include/stdio.h
+pru0adc.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-pru_2.1.5/include/linkage.h
+pru0adc.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-pru_2.1.5/include/stdarg.h
+
+../pru0adc.c: 
+../resource_table_0.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-pru_2.1.5/include/stddef.h: 
+C:/Users/a0232053/AppData/Local/Microsoft/Windows/INetCache/IE/1VP0HB23/pru-software-support-package-pru-software-support-package-master/pru-software-support-package-pru-software-support-package/include/rsc_types.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-pru_2.1.5/include/stdint.h: 
+C:/Users/a0232053/AppData/Local/Microsoft/Windows/INetCache/IE/1VP0HB23/pru-software-support-package-pru-software-support-package-master/pru-software-support-package-pru-software-support-package/include/pru_types.h: 
+C:/Users/a0232053/AppData/Local/Microsoft/Windows/INetCache/IE/1VP0HB23/pru-software-support-package-pru-software-support-package-master/pru-software-support-package-pru-software-support-package/include/pru_virtio_ids.h: 
+C:/Users/a0232053/AppData/Local/Microsoft/Windows/INetCache/IE/1VP0HB23/pru-software-support-package-pru-software-support-package-master/pru-software-support-package-pru-software-support-package/include/am335x/pru_cfg.h: 
+C:/Users/a0232053/AppData/Local/Microsoft/Windows/INetCache/IE/1VP0HB23/pru-software-support-package-pru-software-support-package-master/pru-software-support-package-pru-software-support-package/include/am335x/pru_intc.h: 
+C:/Users/a0232053/AppData/Local/Microsoft/Windows/INetCache/IE/1VP0HB23/pru-software-support-package-pru-software-support-package-master/pru-software-support-package-pru-software-support-package/include/pru_rpmsg.h: 
+C:/Users/a0232053/AppData/Local/Microsoft/Windows/INetCache/IE/1VP0HB23/pru-software-support-package-pru-software-support-package-master/pru-software-support-package-pru-software-support-package/include/am335x/../pru_virtqueue.h: 
+C:/Users/a0232053/AppData/Local/Microsoft/Windows/INetCache/IE/1VP0HB23/pru-software-support-package-pru-software-support-package-master/pru-software-support-package-pru-software-support-package/include/pru_virtio_ring.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-pru_2.1.5/include/stdio.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-pru_2.1.5/include/linkage.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-pru_2.1.5/include/stdarg.h: 
---
+++ Debug/subdir_vars.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../AM335x_PRU.cmd 
+
+C_SRCS += \
+../pru0adc.c 
+
+OBJS += \
+./pru0adc.obj 
+
+C_DEPS += \
+./pru0adc.d 
+
+C_DEPS__QUOTED += \
+"pru0adc.d" 
+
+OBJS__QUOTED += \
+"pru0adc.obj" 
+
+C_SRCS__QUOTED += \
+"../pru0adc.c" 
+
+
